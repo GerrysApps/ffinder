647c7ed1c10240cd253ec044b9da9dc7ddcc080d
Gerry Haus <gerry.ha@proton.me>
2025-02-16 10:45:53 -0800

    add license

--- lib/cache.dart
+++ lib/cache.dart
@@ -1,6 +1,30 @@
+/*
+ * ffinder Copyright (c) 2023-2025 GerrysApps.com  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This program is also subject to the following additional terms under
+ * GPLv3 Section 7:
+ *
+ *   If you distribute this or a modified version of this program, you must
+ *   include a prominent notice stating that the original version of this
+ *   software, ffinder, is available at GerrysApps.com. This notice 
+ *   must be reasonably prominent, such as a notice displayed within the
+ *   "About" section of the application or in accompanying documentation.
+ */
 // ignore_for_file: avoid_print, curly_braces_in_flow_control_structures, unused_import
 
-// Copyright (c) 2023-2025 GerrysApps.com  
 import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,30 @@
-// ignore_for_file: curly_braces_in_flow_control_structures, avoid_print, non_constant_identifier_names, prefer_final_fields
+/*
+ * ffinder Copyright (c) 2023-2025 GerrysApps.com  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This program is also subject to the following additional terms under
+ * GPLv3 Section 7:
+ *
+ *   If you distribute this or a modified version of this program, you must
+ *   include a prominent notice stating that the original version of this
+ *   software, ffinder, is available at GerrysApps.com. This notice 
+ *   must be reasonably prominent, such as a notice displayed within the
+ *   "About" section of the application or in accompanying documentation.
+ */
 
-// Copyright (c) 2023-2025 GerrysApps.com  
+// ignore_for_file: curly_braces_in_flow_control_structures, avoid_print, non_constant_identifier_names, prefer_final_fields
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:yaml/yaml.dart';
